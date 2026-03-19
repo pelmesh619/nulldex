36c8138096eb590d2bd6fc9c1e11889403724341
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-19 18:42:05 +0300

    Update MainActivity.kt

--- app/src/main/java/org/pelmeshke/nulldex/MainActivity.kt
+++ app/src/main/java/org/pelmeshke/nulldex/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.pelmeshke.nulldex
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +13,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import org.pelmeshke.nulldex.databinding.ActivityMainBinding
+import org.pelmeshke.nulldex.ui.list.PokemonListFragmentDirections
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -29,11 +31,38 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
 
+        binding.fabFavorites.setOnClickListener {
+            startActivity(Intent(this, FavoriteActivity::class.java))
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.pokemonDetailFragment) {
+                binding.fabFavorites.hide()
+            } else {
+                binding.fabFavorites.show()
+            }
+        }
+
+        intent.getStringExtra("pokemon_name")?.let { name ->
+            navController.navigate(
+                PokemonListFragmentDirections.actionListFragmentToDetailFragment(name)
+            )
+        }
+
         binding.root.post {
             val systemBars =
                 WindowInsetsCompat.toWindowInsetsCompat(window.decorView.rootWindowInsets)
                     .getInsets(WindowInsetsCompat.Type.systemBars())
             binding.root.setPadding(0, systemBars.top, 0, systemBars.bottom)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        intent.getStringExtra("pokemon_name")?.let { name ->
+            navController.navigate(
+                PokemonListFragmentDirections.actionListFragmentToDetailFragment(name)
+            )
         }
     }
 
