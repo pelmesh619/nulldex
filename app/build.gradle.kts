dae8b1c5ca93362148c553befe8f4aab8303c779
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-04-02 14:19:30 +0300

    Add androidx.swiperefreshlayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.swiperefreshlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
