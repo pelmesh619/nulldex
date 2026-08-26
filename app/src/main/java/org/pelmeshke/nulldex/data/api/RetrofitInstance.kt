6557d57d594972cf868d365e7099022ae2353e84
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-08-26 11:38:11 +0300

    Update RetrofitInstance.kt

--- app/src/main/java/org/pelmeshke/nulldex/data/api/RetrofitInstance.kt
+++ app/src/main/java/org/pelmeshke/nulldex/data/api/RetrofitInstance.kt
@@ -1,10 +1,20 @@
 package org.pelmeshke.nulldex.data.api
 
+import okhttp3.OkHttpClient
+import org.pelmeshke.nulldex.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
+    private const val UI_BASE_URL = BuildConfig.UI_BASE_URL
+
+    private val uiClient = OkHttpClient.Builder()
+        .connectTimeout(500, TimeUnit.MILLISECONDS)
+        .readTimeout(200, TimeUnit.MILLISECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     val api: PokeApiService by lazy {
         Retrofit.Builder()
@@ -12,5 +22,23 @@ object RetrofitInstance {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(PokeApiService::class.java)
+    }
+
+    val uiApi: UIConfigService by lazy {
+        Retrofit.Builder()
+            .baseUrl(UI_BASE_URL)
+            .client(uiClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UIConfigService::class.java)
+    }
+
+    val analyticsApi: AnalyticsService by lazy {
+        Retrofit.Builder()
+            .baseUrl(UI_BASE_URL)
+            .client(uiClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AnalyticsService::class.java)
     }
 }
