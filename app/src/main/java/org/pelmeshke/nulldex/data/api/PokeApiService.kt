85f153211bcc8ab9235d0381d2e360a8564d8295
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-08-27 01:12:12 +0300

    Update PokeApiService.kt

--- app/src/main/java/org/pelmeshke/nulldex/data/api/PokeApiService.kt
+++ app/src/main/java/org/pelmeshke/nulldex/data/api/PokeApiService.kt
@@ -3,6 +3,7 @@ package org.pelmeshke.nulldex.data.api
 import org.pelmeshke.nulldex.data.model.Pokemon
 import org.pelmeshke.nulldex.data.model.PokemonList
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -10,11 +11,13 @@ interface PokeApiService {
     @GET("pokemon")
     suspend fun getPokemonList(
         @Query("limit") limit: Int = 20,
-        @Query("offset") offset: Int = 0
+        @Query("offset") offset: Int = 0,
+        @Header("Cache-Control") cacheControl: String? = null
     ): PokemonList
 
     @GET("pokemon/{name}")
     suspend fun getPokemon(
-        @Path("name") name: String
+        @Path("name") name: String,
+        @Header("Cache-Control") cacheControl: String? = null
     ): Pokemon
 }
